c68b15dd65ad87a6b3d39448ddd1ad9da61e3cae
Gizmo900 <jahromia75@gmail.com>
2026-02-07 12:23:59 +0000

    Add files via upload

---
+++ Parsa Date and Time 31st Jan.py
@@ -0,0 +1,12 @@
+
+
+#This program gets the current date and time from the computer and prints it on the screen.
+
+from datetime import datetime  
+# This imports the datetime class from Python’s built-in datetime module
+
+current_datetime = datetime.now()  
+# This gets the current date and time from the computer system and stores it in current_datetime
+
+print(current_datetime)  
+# This displays the current date and time on the screen
